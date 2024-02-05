2e610be2e1abf4a8a91d5e75c96531e44f5ebe41
Kriteak <132823363+Kriteak@users.noreply.github.com>
2024-02-05 21:48:08 +0530

    Update main.py
    Calculator code

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
+print("Coded by Kriteak Raj")
+print("********WELCOME TO CALCULATOR********")
 
-print("Select operation:")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
+a = float(input("Enter the First number: "))
+b = float(input("Enter the second number: "))
+
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
 print("4.Divide")
 
-choice = input("Enter choice(1/2/3/4): ")
+choice = input("Enter what you want(1/2/3/4): ")
 
-if choice == '1':
-  print(a + b)
-elif choice == '2':
-  print(a - b)
-elif choice == '3':
-  print(a * b)
-elif choice == '4':
-  print(a / b)
-else:
-  print("Invalid input")
+if choice == '1' :
+    print(a + b)
+elif choice == '2' :
+    print(a - b)
+elif choice == '3' :
+    print(a * b)
+elif choice == '4' :
+    print(a / b)
+else : 
+    print("The thing you entered is incorrect.Please check your input.")
